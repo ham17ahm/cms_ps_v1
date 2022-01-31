d7499a2ea7c6f2b606c200e2e48f162b72a6e067
ham17ahm <hamadahmadmobeen7@gmail.com>
2022-01-31 01:16:17 +0000

    search letter functionality added to cms

--- admin/search_letters.php
+++ admin/search_letters.php
@@ -7,29 +7,28 @@
 <div id="page-wrapper">
     <div class="container-fluid">
 
-        <?php
-
-        $query = "SELECT * FROM letters";
-
-        $select_all_letters_qry = mysqli_query($conn, $query);
-
-        ?>
-
         <!-- Page Heading -->
         <div class="row">
             <div class="col-lg-12">
+
                 <h1 class="page-header">
                     Search Letters
                     <small>PS Office</small>
                 </h1>
-                <!-- <form action="" method="get">
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control" name="search" placeholder="Search...">
-                        <div class="input-group-append">
-                            <button type="submit" class="btn btn-primary">Search</button>
-                        </div>
+
+                <form action="" method="get">
+                    <div class="input-group form-group">
+                        <input type="text" class="form-control" name="search" placeholder="Search..." value="<?php if (isset($_GET['search'])) {
+                                                                                                                    echo $_GET['search'];
+                                                                                                                } ?>">
+                        <span class="input-group-btn">
+                            <button type="submit" class="btn btn-default btn-primary" type="button">
+                                <span class="glyphicon glyphicon-search"></span>
+                            </button>
+                        </span>
                     </div>
-                </form> -->
+                </form>
+
                 <table class="table table-bordered">
                     <thead>
                         <tr>
@@ -41,28 +40,40 @@
                         </tr>
                     </thead>
                     <tbody>
+                        <?php
+
+                        if (isset($_GET["search"])) {
+                            $filter_values = $_GET["search"];
+
+                            $query = "SELECT * FROM letters WHERE CONCAT(letter_full_name, letter_footnote, letter_tags) LIKE '%{$filter_values}%'";
+
+                            $letter_search_qry = mysqli_query($conn, $query);
+
+                            if (mysqli_num_rows($letter_search_qry) > 0) {
+                                foreach ($letter_search_qry as $row) {
+                        ?>
+
+                                    <tr>
+                                        <td><?php echo $row["letter_id"]; ?></td>
+                                        <td><?php echo $row["letter_date"]; ?></td>
+                                        <td><?php echo $row["letter_full_name"]; ?></td>
+                                        <td style='white-space:pre;'><?php echo $row["letter_footnote"]; ?></td>
+                                        <td><?php echo $row["letter_tags"]; ?></td>
+                                    </tr>
+
+                                <?php
+                                }
+                            } else {
+                                ?>
+
+                                <tr>
+                                    <td colspan="5">Sorry. No Data Found!</td>
+                                </tr>
 
                         <?php
-                        while ($row = mysqli_fetch_assoc($select_all_letters_qry)) {
-                            $letter_id = $row["letter_id"];
-                            $letter_ref = $row["letter_ref"];
-                            $letter_date = $row["letter_date"];
-                            $letter_name_prefix = $row["letter_name_prefix"];
-                            $letter_full_name = $row["letter_full_name"];
-                            $letter_pre_text = $row["letter_pre_text"];
-                            $letter_content = $row["letter_content"];
-                            $letter_footnote = $row["letter_footnote"];
-                            $letter_print_head = $row["letter_print_head"];
-                            $letter_tags = $row["letter_tags"];
-
-                            echo "<tr>";
-                            echo "<td>{$letter_id}</td>";
-                            echo "<td>{$letter_date}</td>";
-                            echo "<td>{$letter_full_name}</td>";
-                            echo "<td>{$letter_tags}</td>";
-                            echo "<td style='white-space:pre;'>{$letter_footnote}</td>";
-                            echo "</tr>";
+                            }
                         }
+
                         ?>
 
                     </tbody>
